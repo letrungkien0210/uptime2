db2cd8d4602d087403682771cec62af50b74db2c
letrungkien0210 <letrungkien0210@gmail.com>
2017-01-04 05:44:03 +0700

    Update the new UI for create/edit event page.

--- app/dashboard/public/javascripts/checkState.js
+++ app/dashboard/public/javascripts/checkState.js
@@ -3,33 +3,34 @@ var updateCheckState = function(check) {
   if (typeof check.isUp == 'undefined') {
     if (check.lastTested) {
       // recover from pause
-      html += '<span class="label label-info">unknown</span>';
+      html += '<span class="label bg-blue bg-font-blue">unknown</span>';
     } else {
       // never tested
-      html += '<span class="label label-important">down</span> <span class="label label-warning">new</span>';
+      // html += '<span class="label label-important">down</span> <span class="label label-warning">new</span>';
+      html += '<span class="label bg-red-mint bg-font-red-mint">down</span><span class="font-red-mint font-lg">new</span>'
     }
   } else {
     var status = {};
     if (check.isPaused) {
       // paused
-      status.label = 'info';
-      status.color = 'blue';
+      status.label = 'bg-blue bg-font-blue';
+      status.color = 'font-blue font-lg';
       status.text = 'paused';
       status.date = check.lastTested;
     } else if (check.isUp) {
       // up
-      status.label = 'success';
-      status.color = 'green';
+      status.label = 'bg-green-jungle bg-font-green-jungle';
+      status.color = 'font-green-jungle font-lg';
       status.text = 'up';
       status.date = check.lastChanged;
     } else {
       // down
-      status.label = 'important';
-      status.color = 'red';
+      status.label = 'bg-red-mint bg-font-red-mint';
+      status.color = 'font-red-mint font-lg';
       status.text = 'down';
       status.date = check.lastChanged;
     }
-    html += '<span class="label label-' + status.label + '">' +  status.text +'</span>'
+    html += '<span class="label ' + status.label + '">' +  status.text +'</span>'
     html += '<span class="' + status.color + '"> for <span title="' + new Date(status.date) + '">' + moment(status.date).fromNow(true) + '</span></span>';
   }
   $('#check_24h').html(html);
